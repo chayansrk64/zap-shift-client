c13683c99b8288cc983775201e9e185ca1e766ba
chayansrk64 <chayansrk64@gmail.com>
2025-11-18 00:52:35 +0600

    send parcel form created

--- src/pages/SendParcel/SendParcel.jsx
+++ src/pages/SendParcel/SendParcel.jsx
@@ -37,18 +37,54 @@ const SendParcel = () => {
                     </fieldset>
                 </div>
                 {/* two column: sender and recevier details */}
-                <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
                     {/* sender info */}
-                    <div>
-                        sender
-                    </div>
-                    {/* recever info */}
-                    <div>
-                        recever
-                    </div>
+                     <fieldset className="fieldset">
+                        {/* sender name */}
+                        <h4 className="text-xl font-semibold">Sender Details</h4>
+                        <label className="label">Sender Name</label>
+                        <input type="text" {...register('senderName')} className="input w-full" placeholder="Sender Name" />
+                        {/* wire house */}
+                        <label className="label">Sender PickUp Wire House</label>
+                        <input type="text" {...register('senderWireHouse')} className="input w-full" placeholder="Sender Wire House" />
+                        {/* sender address */}
+                        <label className="label">Sender Address</label>
+                        <input type="text" {...register('senderAddress')} className="input w-full" placeholder="Sender Address" />
+                        {/* sender contact number */}
+                        <label className="label">Sender Contact Number</label>
+                        <input type="number" {...register('senderContactNumber')} className="input w-full" placeholder="Sender Contact Number" />
+                        {/* sender region */}
+                        <label className="label">Sender Region</label>
+                        <input type="text" {...register('senderRegion')} className="input w-full" placeholder="Sender Region" />
+                        {/* picup instruction */}
+                        <label className="label">Picup Instructions</label>
+                         <textarea {...register('senderPicupInstructions')} className="textarea h-24 w-full" placeholder="Picup Instructions"></textarea>
+                    </fieldset>
+                    {/* Receiver info */}
+                     <fieldset className="fieldset">
+                        {/* receiver name */}
+                        <h4 className="text-xl font-semibold">Receiver Details</h4>
+                        <label className="label">Receiver Name</label>
+                        <input type="text" {...register('receiverName')} className="input w-full" placeholder="Receiver Name" />
+                        {/* wire house */}
+                        <label className="label">Receiver PickUp Wire House</label>
+                        <input type="text" {...register('receiverWireHouse')} className="input w-full" placeholder="Receiver Delivery Wire House" />
+                        {/* Receiver address */}
+                        <label className="label">Receiver Address</label>
+                        <input type="text" {...register('receiverAddress')} className="input w-full" placeholder="Receiver Address" />
+                        {/* Receiver contact number */}
+                        <label className="label">Receiver Contact Number</label>
+                        <input type="number" {...register('receiverContactNumber')} className="input w-full" placeholder="Receiver Contact Number" />
+                        {/* Receiver region */}
+                        <label className="label">Receiver Region</label>
+                        <input type="text" {...register('receiverRegion')} className="input w-full" placeholder="Receiver Region" />
+                        {/* picup instruction */}
+                        <label className="label">Picup Instructions</label>
+                         <textarea {...register('receiverPicupInstructions')} className="textarea h-24 w-full" placeholder="Picup Instructions"></textarea>
+                    </fieldset>
                 </div>
                 {/* proceed button */}
-                <input type="submit" value="Send Parcel" className="btn btn-primary text-black" />
+                <input type="submit" value="Send Parcel" className="btn btn-primary w-full text-black my-8" />
             </form>
         </div>
     );
