9bfa0aaccf5aadb22b961d6291c0ced22b078125
chayansrk64 <chayansrk64@gmail.com>
2025-11-24 01:35:42 +0600

    interceptor added in the uesAxiosSecure hook

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,46 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:3000'
 })
 
 const useAxiosSecure = () => {
+    const { user, logOut } = useAuth()
+    const  navigate = useNavigate()
+
+    useEffect(() => {
+
+        // request interceptor
+        const reqestInterceptor = axiosSecure.interceptors.request.use((config) => {
+        config.headers.Authorization = `Bearer ${user.accessToken}`
+        return config
+    })
+
+    // response interceptor 
+        const responseInterceptor = axiosSecure.interceptors.response.use((response)=> {
+            return response;
+        }, (error)=> {
+            console.log(error);
+            const statusCode = error.status;
+            if(statusCode === 401 || statusCode === 403){
+                logOut()
+                .then(() => {
+                    navigate('/login')
+                })
+
+            }
+            return Promise.reject(error)
+        })
+
+    return () => {
+        axiosSecure.interceptors.request.eject(reqestInterceptor)
+        axiosSecure.interceptors.response.eject(responseInterceptor)
+    }
+
+    }, [user])
     return axiosSecure;
 };
 
