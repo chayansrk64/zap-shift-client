d0cde3fb419b129098175d66b61bb340c084733c
chayansrk64 <chayansrk64@gmail.com>
2025-11-29 10:40:00 +0600

    parcel tracking Id send from client

--- src/pages/ParcelTrack/ParcelTrack.jsx
+++ src/pages/ParcelTrack/ParcelTrack.jsx
@@ -25,9 +25,12 @@ const ParcelTrack = () => {
             <p>{trackings?.length}</p>
 
             {/* time line */}
-            <ul className="timeline timeline-vertical">
-  <li>
-    <div className="timeline-start">1984</div>
+  <ul className="timeline timeline-vertical">
+    {
+      trackings.map(log => <li key={log._id}>
+    <div className="timeline-start">
+      {new Date(log.createdAt).toLocaleString()}
+    </div>
     <div className="timeline-middle">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -42,88 +45,12 @@ const ParcelTrack = () => {
         />
       </svg>
     </div>
-    <div className="timeline-end timeline-box">First Macintosh computer</div>
-    <hr />
-  </li>
-  <li>
-    <hr />
-    <div className="timeline-start">1998</div>
-    <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5"
-      >
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd"
-        />
-      </svg>
+    <div className="timeline-end timeline-box">
+      <span className='text-xl'>{log.details}</span>
     </div>
-    <div className="timeline-end timeline-box">iMac</div>
     <hr />
-  </li>
-  <li>
-    <hr />
-    <div className="timeline-start">2001</div>
-    <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5"
-      >
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </div>
-    <div className="timeline-end timeline-box">iPod</div>
-    <hr />
-  </li>
-  <li>
-    <hr />
-    <div className="timeline-start">2007</div>
-    <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5"
-      >
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </div>
-    <div className="timeline-end timeline-box">iPhone</div>
-    <hr />
-  </li>
-  <li>
-    <hr />
-    <div className="timeline-start">2015</div>
-    <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5"
-      >
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </div>
-    <div className="timeline-end timeline-box">Apple Watch</div>
-  </li>
+  </li>)
+    }
 </ul>
 
         </div>
